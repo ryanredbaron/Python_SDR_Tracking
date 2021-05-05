fca69d0417d98c4ee2cfb5e64992d52a6e7f964b
ryanredbaron <ryanredbaroN@gmail.com>
2021-05-05 15:47:51 -0700

    fixed local map

--- SDRComms.py
+++ SDRComms.py
@@ -46,7 +46,8 @@ CoolAirPlaneList = {
     'N9187' : 'Catalina Delivery',
     'N9680B' : 'Catalina Delivery',
     'SLAM' : 'Military Transport?',
-    'KNIFE' : 'Military Helicopter'
+    'KNIFE' : 'Military Helicopter',
+    'N140HP' : 'CHP'
     #,'SWA' : 'Test'
     }
 
