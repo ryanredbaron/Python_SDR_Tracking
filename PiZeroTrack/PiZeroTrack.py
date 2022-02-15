a8d3a1eec72c5ed823eb82ab0adf3e6c8b7dc1b7
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-15 11:32:04 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -12,11 +12,20 @@ import os
 import math
 
 from guizero import App, Drawing
+
 CurrentLat = 33.792641
 CurrentLong = -118.115471
 
-SquareSize = 500
-a = App(title="My app", height=SquareSize, width=SquareSize)
+ScreenHeight = 640
+ScreenWidth = 480
+
+MiddleHeight = ScreenHeight/2
+MiddleWidth = ScreenWidth/2
+
+MapRadius = 25
+MilesPerLat = 0.0145054945054945
+
+a = App(title="PiFlight Track", height=ScreenHeight, width=ScreenWidth)
 
 os.chdir("C:\\Users\\ryanr\\Documents\\GitHub\\Python_SDR_Tracking\\WinSDR")
 cmd = "dump1090.bat"
@@ -34,12 +43,21 @@ def RTLData():
     global process
     
     d.clear()
-    d.image(0, 0, r"C:\Users\ryanr\Desktop\test7.png", width=SquareSize, height=SquareSize)
-    d.oval((SquareSize/2)-10, (SquareSize/2)-10, (SquareSize/2)+10, (SquareSize/2)+10, color=None, outline=2, outline_color="red")
+    d.image(0, 0, r"C:\Users\ryanr\Desktop\test9.png", height=ScreenHeight, width=ScreenWidth)
+    RadarRing = 1
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
+    RadarRing = 5
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
+    RadarRing = 10
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
+    RadarRing = 15
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
+    RadarRing = 20
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
+    RadarRing = 25
+    d.oval((ScreenWidth/2)-((RadarRing*MapRadius)/2), (ScreenHeight/2)-((RadarRing*MapRadius)/2), (ScreenWidth/2)+((RadarRing*MapRadius)/2), (ScreenHeight/2)+((RadarRing*MapRadius)/2), color=None, outline=2, outline_color="red")
 
-    
     Processing = True
-    
     while Processing == True:
         try:
             output = ""
@@ -65,17 +83,30 @@ def RTLData():
                     AirplaneDict.update({SFlight : [SHex,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})
                    
                     for k, v in AirplaneDict.items():
-                        DisplayLong = (SquareSize/2) + ((CurrentLat - v[4]) * 1000)
-                        DisplayLat = (SquareSize/2) - ((CurrentLong - v[5]) * 1000)
+                        #set a max lat/long that you want to see and scale from there
+                        #Max lat = Current lat * Miles
+                        #ScaleLat = Airplanelat/MaxLat = % of max
+                        #MaxMiles * ScaleLat
+                        
+                        #(ScreenWidth/2)-((RadarRing*MapRadius)/2)
+                        DisplayLong = ScreenWidth+(ScreenWidth*(((CurrentLat - v[4])*69)/MapRadius))
+                        DisplayLat = ScreenHeight-(ScreenHeight*(((CurrentLong - v[5])*69)/MapRadius))                  
+                        
+                        '''
+                        DisplayLong =  (ScreenHeight/2) + ((CurrentLat - v[4]) * (MapRadius/MilesPerLat))
+                        DisplayLat = (ScreenWidth/2) + ((CurrentLong - v[5]) * (MapRadius/MilesPerLat))
+                        '''
+                        print(DisplayLat)
+                        print(DisplayLong)
+                        print("---------")
                         d.text(DisplayLat-15,DisplayLong-25,k,size=8)
                         d.oval(DisplayLat-5, DisplayLong-5, DisplayLat+5, DisplayLong+5, color=None, outline=2, outline_color="blue")
-                        
-                        Radius = v[2]/10
+                        SpeedRadius = v[2]/10
                         Heading = ((v[3]-90)%360)*(0.017453)
                         X1 = DisplayLat
                         Y1 = DisplayLong
-                        X2 = (Radius)*(math.cos(Heading))+DisplayLat
-                        Y2 = (Radius)*(math.sin(Heading))+DisplayLong
+                        X2 = (SpeedRadius)*(math.cos(Heading))+DisplayLat
+                        Y2 = (SpeedRadius)*(math.sin(Heading))+DisplayLong
                         d.line(X1,Y1,X2,Y2,color="black",width=2)
                 #Clean up dictionary
                     try:
@@ -94,7 +125,7 @@ def RTLData():
             sys.exit(0)
     
 
-d = Drawing(a, width=SquareSize, height=SquareSize)
+d = Drawing(a, height=ScreenHeight, width=ScreenWidth)
 d.repeat(100, RTLData)
 
 a.display()
