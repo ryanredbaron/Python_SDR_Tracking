b885a0d93f8a4d6700cfc8544aa3d18080d07c3d
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 13:12:04 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -114,7 +114,9 @@ def RTLData():
                             pass
                                                         #0    1     2   3     4   5       6
                         #AirplaneDict.update({SFlight : [SHex,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})
-                       
+                        print(SFlight)
+                        print(AirplaneDict[SFlight])
+                        print("-------------------")
                 for k, v in AirplaneDict.items():
                     DisplayLong = (ScreenHeight/2)+(ScreenWidth*(((CurrentLat - v[4])*69)/MapRadius))
                     DisplayLat = (ScreenWidth/2)-(ScreenHeight*(((CurrentLong - v[5])*69)/MapRadius))                  
