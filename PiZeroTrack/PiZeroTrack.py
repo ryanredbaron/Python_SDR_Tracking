201b67c44f0e9a11ea3ec701d68d329ab1f0c09f
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 13:44:38 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -67,7 +67,7 @@ def RTLData():
     while Processing == True:
         try:
             output = ""
-            output = (process.stdout.readline())
+            output = (process.stdout.readline()).decode()
             if output:
                 data = output.split("\n")
                 #print(data)
