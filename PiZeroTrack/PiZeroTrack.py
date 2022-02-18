e116976da0f4a8ea15fe0543fd8427f1132cdcd5
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-17 23:38:55 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -74,16 +74,34 @@ def RTLData():
                 if output.startswith("-----------------------------------"):
                     continue
                 if len(ParseOutput) == 12 and ParseOutput[0] != "(INCLUDING" and ParseOutput[0] != "OF":
-                    SHex = ParseOutput[0]
-                    SFlight = ParseOutput[3]
+                    try:
+                        SHex = ParseOutput[0]
+                    except:
+                        SHex = "0"
+                    try:
+                        SFlight = ParseOutput[3]
+                    except:
+                        SFlight = "0"
                     try:
                         SAlt = int(ParseOutput[4])
                     except:
                         SAlt = 0
-                    SSpd = int(ParseOutput[5])
-                    SHdg = int(ParseOutput[6])
-                    SLat = float(ParseOutput[7])
-                    SLong = float(ParseOutput[8])
+                    try:
+                        SSpd = int(ParseOutput[5])
+                    except:
+                        SSpd = 0
+                    try:
+                        SHdg = int(ParseOutput[6])
+                    except:
+                        SHdg = 0
+                    try:
+                        SLat = float(ParseOutput[7])
+                    except:
+                        SLat = 0
+                    try:
+                        SLong = float(ParseOutput[8])
+                    except:
+                        SLong = 0
                                                     #0    1     2   3     4   5       6
                     AirplaneDict.update({SFlight : [SHex,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})
                    
