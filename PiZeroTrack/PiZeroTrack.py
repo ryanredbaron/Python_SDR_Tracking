72e5a11cb54ff97227939ef10ed994b740ba7a46
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 13:38:59 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -72,10 +72,11 @@ def RTLData():
                 data = output.split("\n")
                 #print(data)
                 for d in data:
-                    line = d.split(",")
+                    line = d.split("     ")
                     print(line)
+                    print("----")
                     len(line)
-                    print("---------")
+                    print("-------------------")
                 
                                   #0    1     2   3     4   5       6
                                   #AirplaneDict.update({SFlight : [SHex,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})
