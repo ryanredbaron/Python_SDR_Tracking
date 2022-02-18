4ac596a8f3f67f7dbd8b7b104735747f53d50fb3
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 00:34:40 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -68,6 +68,8 @@ def RTLData():
             output = ""
             output = (process.stdout.readline()).decode()
             ParseOutput = list(filter(None, output.split(" ")))
+            print(ParseOutput)
+            print("----------------")
             if output:
                 DoNotPass = 0
                 try:
