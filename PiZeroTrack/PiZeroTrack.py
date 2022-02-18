47b76f22193e1e30b68ba1bbc94915043742b4ca
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 12:14:12 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -71,7 +71,7 @@ def RTLData():
             if output:
                 for EachLine in LineSplit:
                     ParseOutput = list(EachLine.split(" "))
-                    if ParseOutput[0] == "" and '/' not in ParseOutput[0] and '/' not in ParseOutput[1]:
+                    if ParseOutput[0] == "" and '/' not in ParseOutput[0] and "'\'" not in ParseOutput[0] and '/' not in ParseOutput[1]:
                         print(ParseOutput)
                         print("--")
                         print(len(ParseOutput))
