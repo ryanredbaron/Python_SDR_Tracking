248a54983e4591470beecb6cfb005da4cf277ccf
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 10:54:28 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -71,7 +71,7 @@ def RTLData():
             if output:
                 for EachLine in LineSplit:
                     ParseOutput = list(EachLine.split(" "))
-                    print(ParseOutput[0])
+                    print(ParseOutput)
                     print("---------------")
                     try:SHex = ParseOutput[0]
                     except:SHex = 0
