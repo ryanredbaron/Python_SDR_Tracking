c337170ecbc3ec28e217725afb2f31b8af96db0e
Ryan Henderson <Ryan.Henderson.Contact@gmail.com>
2022-02-18 16:22:52 -0800

    Update PiZeroTrack.py

--- PiZeroTrack/PiZeroTrack.py
+++ PiZeroTrack/PiZeroTrack.py
@@ -107,12 +107,13 @@ def RTLData():
                             AirplaneDict[SelectedAircraft][5] =  InspectionItem 
                                                            #0       1   2   3      4    5      6
                             #AirplaneDict.update({SHex : [SFlight,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})    
-                    print(AirplaneDict)
-                    print("----------------")
+
                     CreatedList = []
                 else:
                     PacketReady = 0
-                    
+                
+                print(AirplaneDict)
+                print("----------------")
                                                                   #0    1     2   3     4   5       6
                                   #AirplaneDict.update({SFlight : [SHex,SAlt,SSpd,SHdg,SLat,SLong,int(time.time())]})
                 for k, v in AirplaneDict.items():
